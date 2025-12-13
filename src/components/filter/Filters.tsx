f0d7a8afe4404ae4f29d294a05fc8281c826a34b
jardenna <hbjornum@gmail.com>
2025-12-13 12:57:59 +0100

    checkbox

--- src/components/filter/Filters.tsx
+++ src/components/filter/Filters.tsx
@@ -1,7 +1,6 @@
+import { ChangeEvent } from 'react';
 import { useSearchParams } from 'react-router';
 import useFilterParams from './useFilterParams';
-
-type FilterKey = 'category' | 'query';
 
 export const options = [
   { value: 'apple', label: 'Apple' },
@@ -12,16 +11,37 @@ export const options = [
 export default function Filters() {
   const [params, setParams] = useSearchParams();
 
-  const handleChange = (key: FilterKey, value: string) => {
+  function onChange(event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    const { name, value, type } = event.target;
+    const checked = (event.target as HTMLInputElement).checked;
+
     const next = new URLSearchParams(params);
-    if (value === '') {
-      next.delete(key);
-    } else {
-      next.set(key, value);
+
+    if (type !== 'checkbox') {
+      if (value === '') {
+        next.delete(name);
+      } else {
+        next.set(name, value);
+      }
+
+      setParams(next, { replace: true });
+      return;
     }
 
+    const currentValues = next.getAll(name);
+
+    next.delete(name);
+
+    const nextValues = checked
+      ? [...currentValues, value]
+      : currentValues.filter((v) => v !== value);
+
+    nextValues.forEach((v) => {
+      next.append(name, v);
+    });
+
     setParams(next, { replace: true });
-  };
+  }
 
   const filters = useFilterParams();
   console.log(filters);
@@ -32,26 +52,36 @@ export default function Filters() {
       <label htmlFor="category-filter">Category</label>
       <select
         id="category-filter"
+        name="category"
         value={params.get('category') ?? ''}
-        onChange={(e) => {
-          handleChange('category', e.target.value);
-        }}
+        onChange={onChange}
       >
         <option value="">All</option>
         <option value="women">Women</option>
         <option value="men">Men</option>
         <option value="accessories">Accessories</option>
       </select>
+      {/* Fruit*/}
+      <div>
+        <input
+          type="checkbox"
+          id="fruit"
+          name="fruit"
+          value="apple"
+          checked={params.getAll('fruit').includes('apple')}
+          onChange={onChange}
+        />{' '}
+        <label htmlFor="fruit">apple</label>
+      </div>
 
       {/* TEXT SEARCH */}
       <label htmlFor="query-filter">Search</label>
       <input
         id="query-filter"
+        name="query"
         type="search"
         value={params.get('query') ?? ''}
-        onChange={(e) => {
-          handleChange('query', e.target.value);
-        }}
+        onChange={onChange}
         placeholder="Search products"
       />
     </form>
