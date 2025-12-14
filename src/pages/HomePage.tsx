26e16e48d553cb5a8be781dfb5a478708f179aa3
jardenna <hbjornum@gmail.com>
2025-12-14 12:14:30 +0100

    checkboxlist

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,21 @@
-import Filters from '../components/filter/Filters';
+import CheckboxList from '../components/filter/CheckboxList';
+
+const checkboxItems = [
+  { id: 'apple', label: 'Apple' },
+  { id: 'banana', label: 'Banana' },
+  { id: 'orange', label: 'Orange' },
+];
+const defaultChecked = ['apple'];
 
 const HomePage = () => (
   <section>
     <header>
       <h1>Home</h1>
     </header>
-    <Filters />
+    <CheckboxList
+      checkboxItems={checkboxItems}
+      defaultChecked={defaultChecked}
+    />
   </section>
 );
 
